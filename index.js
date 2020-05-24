f0bd071e504a84740bcab3c4c3adb7cc48bfb39a
amit <amit@schooglink.com>
2020-05-24 22:55:40 +0530

    Updated module exports.

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const Container = require('./container');
 
 module.exports = {
     Container: Container,
-    createDecorator: inject.createDecorator,
     createWire: inject.createWire,
     createResolve: inject.createResolve,
     NOCACHE: inject.NOCACHE
